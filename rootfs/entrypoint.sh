92bddffd3373d50b21412a180f2decea9b6653c8
Marcel Beck <marcel@beck.im>
2023-08-17 09:59:17 +0200

    feat: add some output

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -2,21 +2,25 @@
 
 if test "${SSH_REGENERATE_HOST_KEY}" = "true"
 then
+    echo "Regenerating Host Keys"
     rm /etc/ssh/ssh_host_*
     ssh-keygen -A
 fi
 
+echo "Adding user ${SSH_USERNAME}"
 adduser -D -s /bin/ash ${SSH_USERNAME}
 passwd -u ${SSH_USERNAME}
 
 if test -n "${SSH_PASSWORD}"
 then
+    echo "Changing Password for ${SSH_USERNAME}"
     echo "${SSH_USERNAME}:${SSH_PASSWORD}" | chpasswd
 fi
 
 if test -n "${SSH_PUB_KEYS_URL}"
 then
-    mkdir -p /home/${USERNAME}/.ssh
+    echo "Adding SSH Pub Keys for ${SSH_USERNAME} from ${SSH_PUB_KEYS_URL}"
+    mkdir -p /home/${SSH_USERNAME}/.ssh
     curl ${SSH_PUB_KEYS_URL} --output /home/${SSH_USERNAME}/.ssh/authorized_keys
 
     chown -R ${SSH_USERNAME}:${SSH_USERNAME} /home/${SSH_USERNAME}
