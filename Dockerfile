c785977468e5adca36d7a289f496c15f51ef6b61
Marcel Beck <marcel@beck.im>
2023-08-17 10:37:47 +0200

    feat: allow creating dirs for user

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV SSH_USERNAME anon
 ENV SSH_PASSWORD ""
 ENV SSH_PUB_KEYS_URL ""
 ENV SSH_REGENERATE_HOST_KEY true
+ENV USER_DIRS_TO_CREATE ""
 
 RUN apk add --no-cache openssh curl \
     && sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -8,8 +8,18 @@ then
 fi
 
 echo "Adding user ${SSH_USERNAME}"
-adduser -D -s /bin/ash ${SSH_USERNAME}
-passwd -u ${SSH_USERNAME}
+adduser -D -s /bin/ash "${SSH_USERNAME}"
+passwd -u "${SSH_USERNAME}"
+
+if test -n "${USER_DIRS_TO_CREATE}"
+then
+    dirs=$(echo "${USER_DIRS_TO_CREATE}" | tr "," "\n")
+    for dir in ${dirs}
+    do
+        echo "Creating directory /home/${SSH_USERNAME}/${dir}"
+        mkdir -p "/home/${SSH_USERNAME}/${dir}"
+    done
+fi
 
 if test -n "${SSH_PASSWORD}"
 then
@@ -20,12 +30,12 @@ fi
 if test -n "${SSH_PUB_KEYS_URL}"
 then
     echo "Adding SSH Pub Keys for ${SSH_USERNAME} from ${SSH_PUB_KEYS_URL}"
-    mkdir -p /home/${SSH_USERNAME}/.ssh
-    curl ${SSH_PUB_KEYS_URL} --output /home/${SSH_USERNAME}/.ssh/authorized_keys
+    mkdir -p "/home/${SSH_USERNAME}/.ssh"
+    curl "${SSH_PUB_KEYS_URL}" --output "/home/${SSH_USERNAME}/.ssh/authorized_keys"
 
-    chown -R ${SSH_USERNAME}:${SSH_USERNAME} /home/${SSH_USERNAME}
-    chmod 700 /home/${SSH_USERNAME}/.ssh
-    chmod 644 /home/${SSH_USERNAME}/.ssh/authorized_keys
+    chown -R ${SSH_USERNAME}:${SSH_USERNAME} "/home/${SSH_USERNAME}"
+    chmod 700 "/home/${SSH_USERNAME}/.ssh"
+    chmod 644 "/home/${SSH_USERNAME}/.ssh/authorized_keys"
 fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
